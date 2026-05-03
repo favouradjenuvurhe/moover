f1b44fd375966b35b2fcae304d2fc6ba24cde039
Favour Adjenuvurhe <133374851+favouradjenuvurhe@users.noreply.github.com>
2026-05-03 19:26:06 +0100

    Sanitize email parameters in send method

--- includes/class-email.php
+++ includes/class-email.php
@@ -3,6 +3,10 @@
 class Moover_Email {
 
     public static function send($to, $subject, $message) {
-        wp_mail($to, $subject, $message);
+        wp_mail(
+            sanitize_email($to),
+            sanitize_text_field($subject),
+            wp_kses_post($message)
+        );
     }
 }
